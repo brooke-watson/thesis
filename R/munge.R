0fb038ded982ce099ccb2170905422c6cca8c901
Brooke Watson <watson@ecohealthalliance.org>
2016-11-18 12:06:58 -0500

    added line about file encoding problems

--- R/munge.R
+++ R/munge.R
@@ -1,13 +1,20 @@
 #' Munge
 #'
 #' Quick and dirty simple munging for data frames with mostly character data.
+#'
+#' Note - if this throws the error "Error in FUN(X[[i]], ...) : invalid multibyte string 203",
+#' you've got some weird characters in your dataset. Add '  fileEncoding="latin1  ' to the end
+#' of your read.csv line for the data frame that you read in.
 #' @param df matrix or data table
 #' @return lowercase data frame with white space trimmed. numeric, Date, and integer columns are left alone.
 #' @keywords data munging
 #' @export
+#' @
 #' @details many thanks to this stack overflow answer: http://stackoverflow.com/questions/17964513/subset-variables-in-data-frame-based-on-column-type
 #' @examples
-#' df = data.frame(number = numeric(c(1,2,3)), characternumber = c("one ", " TwO", "ThrEe"), city = c(" Memphis", "MEMPHIS", "memphis "))
+#' df = data.frame(number = numeric(c(1,2,3)),
+#'      characternumber = c("one ", " TwO", "ThrEe"),
+#'      city = c(" Memphis", "MEMPHIS", "memphis "))
 #' munge(df)
 
 munge = function(df){
@@ -19,7 +26,7 @@ munge = function(df){
     lapply((df[,!sapply(df, is.numeric) & !sapply(df,is.integer) &
          !sapply(df,is.Date)]), trimws) %>%
     lapply(tolower) %>%
-    as.data.frame(stringsAsFactors = FALSE)
+    as.data.frame(stringsAsFactors = FALSE, fileEncoding="latin1")
 
   return(df)
 }
