08af5ddd73531ba2b13caddb91450e10f4efd2b7
Brooke Watson <watson@ecohealthalliance.org>
2016-11-22 12:20:07 -0500

    convenience wrapper

---
+++ R/nunique.R
@@ -0,0 +1,12 @@
+#' nunique
+#'
+#' Get number of unique values in a vector or column of a dataframe
+#' @param x vector or column of a dataframe
+#' @return atomic number listing number of unique values in x
+#' @examples
+#' vec = c(1,3,4,2,5,5,5,1,1,3,7,9)
+#' nunique(vec)
+
+nunique = function(x){
+  length(unique(x))
+}
